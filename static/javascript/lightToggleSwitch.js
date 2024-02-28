e00e3a884d53c4afeb9c97a8405b23120f141d98
Gigarozdupcator 3000 <zeglen.mateusz0@gmail.com>
2024-02-29 00:05:53 +0100

    - Funkcjonalność aplikacji: - poprawa

--- static/javascript/lightToggleSwitch.js
+++ static/javascript/lightToggleSwitch.js
@@ -1,10 +1,19 @@
 const lightToggle = document.getElementById('checkboxInput')
 const ligths = document.getElementById("content")
+const elements = document.getElementsByClassName("bgs");
 lightToggle.addEventListener('change', () => {
     if(lightToggle.checked){
-        ligths.style.color = "var(--white)"
+        ligths.style.color = "white";
+
+
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.backgroundColor = "var(--bg)";
+        }
     }
     else{
-        ligths.style.color = "var(--bg)"
+        ligths.style.color = "rgba(0, 0, 0, 0)";
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.backgroundColor = "rgba(0, 0, 0, 0)";
+        }
     }
 })
